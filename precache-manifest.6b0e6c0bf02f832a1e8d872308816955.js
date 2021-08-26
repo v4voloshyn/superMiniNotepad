c3dbdbee19b4be6c355ee8028c16db3c0740518e
Voloshyn <v4voloshyn@gmail.com>
2021-08-26 22:54:46 +0300

    Updates

---
+++ precache-manifest.6b0e6c0bf02f832a1e8d872308816955.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4855921883c1b6608041c5b484f82573",
+    "url": "/superMiniNotepad/index.html"
+  },
+  {
+    "revision": "8b4f4bc05db445e75002",
+    "url": "/superMiniNotepad/static/css/2.a5f06b7f.chunk.css"
+  },
+  {
+    "revision": "e0a1a620d133d5ce69e8",
+    "url": "/superMiniNotepad/static/css/main.34cccba5.chunk.css"
+  },
+  {
+    "revision": "8b4f4bc05db445e75002",
+    "url": "/superMiniNotepad/static/js/2.c7234c6c.chunk.js"
+  },
+  {
+    "revision": "e0a1a620d133d5ce69e8",
+    "url": "/superMiniNotepad/static/js/main.faacad86.chunk.js"
+  },
+  {
+    "revision": "d0c20ee39b0a914a35b4",
+    "url": "/superMiniNotepad/static/js/runtime-main.fca26502.js"
+  }
+]);
