ccd9475f56bdffb1693f33e4793de29244caec04
Voloshyn <v4voloshyn@gmail.com>
2021-08-26 13:58:09 +0300

    build version

---
+++ build/precache-manifest.1836ec373f75144a85156e11617e372a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee7d41d069bad33345a8bab2618a62bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b4f4bc05db445e75002",
+    "url": "/static/css/2.a5f06b7f.chunk.css"
+  },
+  {
+    "revision": "e0a1a620d133d5ce69e8",
+    "url": "/static/css/main.34cccba5.chunk.css"
+  },
+  {
+    "revision": "8b4f4bc05db445e75002",
+    "url": "/static/js/2.c7234c6c.chunk.js"
+  },
+  {
+    "revision": "e0a1a620d133d5ce69e8",
+    "url": "/static/js/main.faacad86.chunk.js"
+  },
+  {
+    "revision": "56aa0e7d448785aa2636",
+    "url": "/static/js/runtime-main.06f90e6d.js"
+  }
+]);
